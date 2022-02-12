eb05ae33b52eeba9b67e5eee408f2d0a14dbd87e
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-12 00:44:40 -0300

    added delete, update and find by Id to the controller

--- controllers/verses.js
+++ controllers/verses.js
@@ -52,15 +52,83 @@ exports.findAll = async (req, res) => {
   
 
 // Find a single Verse with an id
-exports.findById = (req, res) => {
+exports.findById = async (req, res) => {
+
+
+      const id = req.params.id;
+      try{
+          const data= await Verse.findByPk(id)
+            
+          if (data) {
+            res.send(data);
+          } else {
+            res.status(404).send({
+              message: `Cannot find Verse with id=${id}.`
+            });
+          }
+        
+      }catch(err) {
+        res.status(500).send({
+          message: "Error retrieving Verse with id=" + id
+        });
+      };
+
+
+
   
 };
 // Update a Verse by the id in the request
-exports.update = (req, res) => {
-  
+exports.update = async (req, res) => {
+       if (req.body==undefined) {
+                    res.status(409).send({
+                    message: "Content can not be empty!"
+                    });
+                    return;
+                }
+      const id = req.params.id;
+      try{
+                const num = await Verse.update(req.body,{where: { id: id }})
+                  
+                if (num == 1) {
+                  res.status(204).send({
+                    message: "Verse was updated successfully!"
+                  });
+                } else {
+                  res.status(409).send({
+                    message: `Cannot update Verse with id=${id}. Maybe Verse was not found!`
+                  });
+                }
+          }
+          catch(err){
+            res.status(500).send({
+              message: "Could not update Verse with id=" + id
+            });
+          };
+    
 };
 // Delete a Verse with the specified id in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
+
+  const id = req.params.id;
+  try{
+          const num = await Verse.destroy({where: { id: id }})
+            
+          if (num == 1) {
+            res.send({
+              message: "Verse was deleted successfully!"
+            });
+          } else {
+            res.send({
+              message: `Cannot delete Verse with id=${id}. Maybe Verse was not found!`
+            });
+          }
+    }
+    catch(err){
+      res.status(500).send({
+        message: "Could not delete Verse with id=" + id
+      });
+    };
+
   
 };
 
