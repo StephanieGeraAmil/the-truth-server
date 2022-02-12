11eca9fe6ca03ab47c0fac924dc53790378f738f
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-12 17:53:40 -0300

    deleting verses router file beacuse is now on index and adding migrations to the other tables of the project

---
+++ migrations/20220212204949-create-deck-verses.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('DeckVerses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      verse_id: {
+        type: Sequelize.INTEGER
+      },
+      deck_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('DeckVerses');
+  }
+};
