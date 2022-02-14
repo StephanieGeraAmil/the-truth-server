8ab36a1d1d3f72846d62af7d22a6c7ecb29687a3
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-14 16:45:06 -0300

    changing the production db info

--- config/config.js
+++ config/config.js
@@ -21,8 +21,8 @@ module.exports=  {
   "production": {
     "username": USER,
     "password": PASSWORD,
-    "database": DB,
-    "host": CONNECTION_URL,
+    "database": "postgres://bjnbxsrsdyevgv:224caa2d0958886c931b2fdfa45e7f517b18a6605cb4d6c0363a4ca3d0d52154@ec2-54-83-21-198.compute-1.amazonaws.com:5432/d1fu84kn5bitld",
+    "host":"https://the-truth.herokuapp.com/",
     "dialect": "postgres"
   }
 }
