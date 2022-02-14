ea73f77811b5ba431610537fd7e4860a035b1378
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-14 15:45:40 -0300

    setting port 5432 if it has no .env

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(express.urlencoded({ extended: true}));
 
 
 // app.use('/verses', routerVerses);
-app.use('/', router);
+app.use('/verses', router);
 app.get('/',(req,res)=>{ res.send('Hello to the Truth Aplication')});
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5432;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
