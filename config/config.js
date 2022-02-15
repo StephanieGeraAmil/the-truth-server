c5d1fbbfa56d3f31574181bfd1f6bd37605faea8
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-15 11:28:30 -0300

    adding DETABASE_URL env variable to config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 const dotenv =require('dotenv');
 dotenv.config();
-const {CONNECTION_URL,USER,PORT,PASSWORD,DB}= process.env;
+const {CONNECTION_URL,USER,PORT,PASSWORD,DB,DATABASE_URL}= process.env;
 
 
 module.exports=  {
   "development": {
+    "port":PORT,
     "username": USER,
     "password": PASSWORD,
     "database": DB,
@@ -12,6 +13,7 @@ module.exports=  {
     "dialect": "postgres"
   },
   "test": {
+    "port":PORT,
     "username": USER,
     "password": PASSWORD,
     "database": DB,
@@ -19,10 +21,10 @@ module.exports=  {
     "dialect": "postgres"
   },
   "production": {
+    "port":PORT,
     "username": USER,
     "password": PASSWORD,
-    "database": "postgres://bjnbxsrsdyevgv:224caa2d0958886c931b2fdfa45e7f517b18a6605cb4d6c0363a4ca3d0d52154@ec2-54-83-21-198.compute-1.amazonaws.com:5432/d1fu84kn5bitld",
-    "host":"the-truth.herokuapp.com/",
+    "database": DATABASE_URL,
     "dialect": "postgres"
   }
 }
