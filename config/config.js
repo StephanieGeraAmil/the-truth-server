72d8522418dd7a5e8f5c4b0e782ad4909da2a890
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-15 22:04:28 -0300

    another try

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dotenv =require('dotenv');
 dotenv.config();
-const {CONNECTION_URL,USER,PORT,PASSWORD,DB,DATABASE_URL}= process.env;
+const {CONNECTION_URL,USER,PASSWORD,DB,DATABASE_URL}= process.env;
 
 
 module.exports=  {
@@ -23,7 +23,7 @@ module.exports=  {
   "production": {
   
     "database": DATABASE_URL,
-    "port":PORT,
+   
     "dialect": "postgres"
   }
 }
