d8ee3990abca3b75f911859985b227ab725a0f3b
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-16 16:32:01 -0300

    trying something else

--- config/config.js
+++ config/config.js
@@ -21,7 +21,12 @@ module.exports=  {
     "dialect": "postgres"
   },
   "production": {
-  
+    "user": "bjnbxsrsdyevgv",
+    "password": "224caa2d0958886c931b2fdfa45e7f517b18a6605cb4d6c0363a4ca3d0d52154",
+    "database": "d1fu84kn5bitld",
+    "host": "ec2-54-83-21-198.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "operatorsAliases": false,
     "use_env_variable": DATABASE_URL,
     "dialectOptions": {
       'ssl': {
