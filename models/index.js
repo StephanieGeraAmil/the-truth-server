0a8ccd65c87389629b3252e7400ee77291d4bc88
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-16 17:45:41 -0300

    still trying4

--- models/index.js
+++ models/index.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  console.log(config);
-  console.log(config.use_env_variable);
-
   sequelize = new Sequelize(config.use_env_variable, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
