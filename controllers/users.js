39215039a2233922988f7769e81566726639c4a4
Stephanie <stephanie.gera.amil@gmail.com>
2022-10-18 12:59:48 -0300

    fixing bug introduced to create when it doesnt exist the user

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,7 @@ const Op = db.sequelize.Op;
 
 exports.create = async (req, res) => {
   // Validate request
-
+ console.log(req);
   if (req.body == undefined) {
     res.status(400).send({
       message: "Content can't be empty!",
@@ -52,6 +52,7 @@ exports.create = async (req, res) => {
   }
   // createUser(req.body.name, req.body.email,res);
   // Create a User
+  console.log(req.body);
   const usr = {
     name: req.body.name,
     email: req.body.email,
@@ -86,68 +87,115 @@ exports.findAll = async (req, res) => {
 };
 
 // Find a single User with an id or an user based on the email if the body isnt empty and if it doesnt exist it will be created and retreived
+// exports.findByParameter = async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     if (!req.body) {
+//       //if no body param is id
+//       console.log("no body");
+
+//       const param = req.params.param;
+//       const data = await User.findByPk(param);
+
+//       if (data) {
+//         res.send(data);
+//       } else {
+//         res.status(404).send({
+//           message: `Can't find User with id=${param}.`,
+//         });
+//       }
+//     } else {
+//       //if body param is email
+//       if (!req.body.name || !req.body.email) {
+//         console.log("body incomplete");
+//            console.log(req.body);
+//         res.status(400).send({
+//           message: " a name and email is required",
+//         });
+//         return;
+//       } else {
+//         console.log("searching");
+//         //const email = req.params;
+//         let data = await User.findAll({
+//           where: {
+//             email: req.params.param,
+//           },
+//         });
+//         console.log(data[0].dataValues);
+//         if (data[0].dataValues) {
+//           console.log("not found");
+//           //there is no user with that email, so I create it
+//           // data = await createUser(req.body.name, req.body.email,res);
+//           // Create a User
+//           const usr = {
+//             name: req.body.name,
+//             email: req.body.email,
+//             id: uuidv4(),
+//             createdAt: new Date(),
+//             updatedAt: new Date(),
+//           };
+
+//           // Save User in the database
+//           // User.create(usr)
+//           //   .then((data) => {
+//           //     res.send(data);
+//           //   })
+//           //   .catch((err) => {
+//           //     res.status(500).send({
+//           //       message:
+//           //         err.message || "Some error occurred while creating the User.",
+//           //     });
+//           //   });
+//         } else {
+//           console.log("found");
+//           //i retreive the user
+//           res.status(200).send(data);
+//         }
+//       }
+//     }
+//   } catch (err) {
+//     res.status(500).send({
+//       message: err,
+//     });
+//   }
+// };
 exports.findByParameter = async (req, res) => {
   try {
-    console.log(req.body);
-    if (req.body == undefined) {
-      //if no body param is id
-      const id = req.params;
-      const data = await User.findByPk(id);
-
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Can't find User with id=${id}.`,
-        });
-      }
-    } else {
-      //if body param is email
-      if (!req.body.name || !req.body.email) {
-        console.log("body incomplete");
-        res.status(400).send({
-          message: " a name and email is required",
-        });
-        return;
-      } else {
-        console.log("searching");
-        //const email = req.params;
-        let data = await User.findAll({
+    console.log(req.params.param.indexOf("@")!==-1);
+    let type="id";
+    if(req.params.param.indexOf("@")!==-1)type="email";
+let email="";
+    let data={};
+   //let param=""
+    if(type==="email"){
+      email = req.params.param;
+      console.log(email)
+       let result = await User.findAll({
           where: {
-            email: req.body.email,
+            email: email,
           },
         });
-        console.log(data[0]);
-        if (data) {
-          console.log("not found");
-          //there is no user with that email, so I create it
-          // data = await createUser(req.body.name, req.body.email,res);
-          // Create a User
-          const usr = {
-            name: req.body.name,
-            email: req.body.email,
-            id: uuidv4(),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          };
+        if(result)data=result[0];
 
-          // Save User in the database
-          User.create(usr)
-            .then((data) => {
-              res.send(data);
-            })
-            .catch((err) => {
-              res.status(500).send({
-                message:
-                  err.message || "Some error occurred while creating the User.",
-              });
-            });
-        } else {
-          console.log("found");
-          //i retreive the user
-          res.status(200).send(data);
-        }
-      }
+    }
+    // let id=""
+    if (type==="id"){
+      id=req.params.param;
+      // console.log(id);
+      data= await User.findByPk(id);
+    }
+      // console.log(data);
+    
+    
+  
+
+    if (data) {
+      res.send(data);
+    } else {
+        res.send([]);
+      // res.status(404).send({
+      //   message: `Can't find User.`,
+      // });
     }
   } catch (err) {
     res.status(500).send({
